7794f0de53b9b5fe94965225f2b10c20e6f6d76f
H L <xerick_cid@outlook.com>
2025-10-08 00:44:37 -0600

    ya quedo tu

--- lib/adaptive_image.dart
+++ lib/adaptive_image.dart
@@ -3,13 +3,9 @@ import 'package:flutter/material.dart';
 
 class AdaptiveImage extends StatelessWidget {
   AdaptiveImage.network(String url, {super.key}) {
-    if (kIsWeb) {
-      _url = Uri.parse(url)
-          .replace(host: 'localhost', port: 8080, scheme: 'http')
-          .toString();
-    } else {
-      _url = url;
-    }
+    // Para web, usar URLs directas de YouTube sin proxy
+    // Para móvil, usar el proxy CORS si está disponible
+    _url = url;
   }
 
   late final String _url;
